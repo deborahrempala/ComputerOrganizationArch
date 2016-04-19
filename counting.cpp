1d969de0d00ac5d5e7a64971a43f5d3bf4e847a7
Deborah Rempala <drempala@lssu.edu>
2016-04-19 18:36:22 -0400

    Added files via upload

---
+++ counting.cpp
@@ -0,0 +1,30 @@
+#include <iomanip>
+#include <iostream>
+#include <cmath>
+
+
+
+using namespace std;
+// start main
+int main()
+{
+  int count;
+  int answer;
+
+
+ for(int i =0; i<+10000000;i++) 
+ {
+	 answer = answer +.03;
+	 count ++;
+	 cout<<answer<<endl;
+	cout<< count<<endl;
+	
+	
+	
+ }	 
+	
+	cout<<answer<<endl;
+	cout<< count<<endl;
+	
+	
+}
